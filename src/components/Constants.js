cdef3ead519cab9e9585b50e0194605d33867e6c
Anyhowclick <howeilun@hotmail.com>
2018-02-22 16:47:38 +0800

    Added new constant: URL address of ropsten etherscan

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -4,3 +4,4 @@ export const ROPSTEN_NETWORK_ID = "3"
 export const RINKEBY_NETWORK_ID = "4"
 export const KOVAN_NETWORK_ID = "42"
 export const MAX_ATTEMPTS = 100
+export const ROPSTEN_ETHERSCAN_LINK = "https://ropsten.etherscan.io/tx/"
