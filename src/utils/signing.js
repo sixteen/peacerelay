9ce64dc00e67a4b68b8eb52b013ea6e1cd28ecdb
Anyhowclick <howeilun@hotmail.com>
2018-04-12 10:40:49 +0800

    Fixed a minor bug

--- src/utils/signing.js
+++ src/utils/signing.js
@@ -1,4 +1,4 @@
-const Buffer = require("buffer");
+const buffer = require("buffer");
 const EthereumTx = require("ethereumjs-tx");
 const secp256k1 = require("secp256k1/elliptic");
 const keccak256 = require("js-sha3").keccak256;
